e35e55a71c359bb8c763213f18fb7abff67d362c
ashchan <ashchan@gmail.com>
2008-06-25 20:57:04 +0800

    removed smtp config (moved to an initializer)

--- config/environment.rb
+++ config/environment.rb
@@ -70,16 +70,3 @@ Rails::Initializer.run do |config|
 
 end
 require "#{RAILS_ROOT}/vendor/plugins/community_engine/engine_config/boot.rb"
-
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.default_charset = "utf-8"
-ActionMailer::Base.smtp_settings = {
-                                      :tls => true,
-                                      :address => "smtp.gmail.com",
-                                      :port => "587",
-                                      :domain => "www.zikii.com",
-                                      :authentication => :plain,
-                                      :user_name => "zikii.com@gmail.com",
-                                      :password => "beenhero!"
-                                      }
